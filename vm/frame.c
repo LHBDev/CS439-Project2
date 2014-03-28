faed852a511740dbf027482033acfa37e75df377
Sivarama Mullapudi <sivakom@cs.utexas.edu>
2014-03-28 15:41:16 -0500

    updated frame.c

--- vm/frame.c
+++ vm/frame.c
@@ -4,20 +4,16 @@
 #include "lib/kernel/hash.h"
 
 //Header prototypes
-struct frame
-{
-	struct hash_elem hash_elem;
-	uint32_t *addr;	
-};
-
 struct hash frame_table;
 
+
+//Put the hash functions directly in the header file ????
 /* Returns a hash value for page p. */
 unsigned
 frame_hash (const struct hash_elem *p_, void *aux UNUSED)
 {
   const struct frame *p = hash_entry (p_, struct frame, hash_elem);
-  return hash_bytes (&p->addr, sizeof p->addr);
+  return hash_bytes (&p->pte, sizeof p->pte);
 }
 
 /* Returns true if page a precedes page b. */
@@ -28,7 +24,7 @@ frame_less (const struct hash_elem *a_, const struct hash_elem *b_,
   const struct frame *a = hash_entry (a_, struct frame, hash_elem);
   const struct frame *b = hash_entry (b_, struct frame, hash_elem);
 
-  return a->addr < b->addr;
+  return a->pte < b->pte;
 }
 
 void
@@ -37,3 +33,15 @@ frame_init (void)
 	hash_init(&frame_table, frame_hash, frame_less, NULL);
 }
 
+//lookup a frame
+void
+frame_lookup (void)
+{
+  struct hash_elem *target;
+
+  target = hash_find(&frame_table, );
+}
+
+//need for later purposes
+//evicting a frame?
+//any more???
--- vm/frame.h
+++ vm/frame.h
@@ -2,7 +2,18 @@
 #include <debug.h>
 #include <stdint.h>
 
+struct frame
+{
+	struct hash_elem hash_elem;
+	struct thread *owner;
+	uint32_t *pte;
+};
+
+/* Hash Functions for Frame Tables */
 unsigned frame_hash(const struct hash_elem *, void * UNUSED);
 bool frame_less(const struct hash_elem *, const struct hash_elem *,
            void * UNUSED);
+
+/* Frame Table functions */
 void frame_init(void);
+void frame_lookup (uint32_t *pte_lookup);
