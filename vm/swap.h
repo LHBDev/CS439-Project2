8b559abd9b5c85066a9603b774ef6b11764a52c9
LHBDev <freeraprb@gmail.com>
2014-04-13 22:41:56 -0500

    test

--- vm/swap.h
+++ vm/swap.h
@@ -1,8 +1,6 @@
 #include <bitmap.h>
-//Put Desired methods, put desired includes, maybe some struct definitions
-//such
 
 //Swap table methods
 void swap_init (void);
-void * swap_in (uint8_t *, int);
-void swap_out (uint8_t *);
+void * swap_in (void *, int);
+void swap_out (void *);
